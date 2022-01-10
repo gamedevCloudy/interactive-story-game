edae5ff4e963118a762d6362317b2ae341aa00ad
Aayush Chaudhary <acaayush1111@gmail.com>
2022-01-10 08:32:45 +0530

    Pattern Grid
    Grid has 9 dots which have their own id.
    Once clicked, they instantiate a line at local transform.

---
+++ Interactive Story Game -  Prototype/Assets/_Scripts/CircleIdentifier.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CircleIdentifier : MonoBehaviour
+{
+    public int id; 
+}
--- Interactive Story Game -  Prototype/Assets/_Scripts/PatternManger.cs
+++ Interactive Story Game -  Prototype/Assets/_Scripts/PatternManger.cs
@@ -4,10 +4,28 @@ using UnityEngine;
 
 public class PatternManger : MonoBehaviour
 {
+    public GameObject linePrefab; 
+    public Canvas canvas; 
+
+    private Dictionary<int,CircleIdentifier> circles;
+    private List<CircleIdentifier> lines; 
+
+    private bool unlocking; 
 
     // Start is called before the first frame update
     void Start()
     {
+        circles = new Dictionary<int, CircleIdentifier>(); 
+        lines = new List<CircleIdentifier>(); 
+        
+        for(int i = 0; i < transform.childCount; i++)
+        {
+            var circle = transform.GetChild(i); 
+            var identifier = circle.GetComponent<CircleIdentifier>(); 
+
+            identifier.id = i; 
+            circles.Add(i, identifier); 
+        }
         
     }
 
@@ -18,5 +36,41 @@ public class PatternManger : MonoBehaviour
         // connect dots
         //verify if correct
         //move to next pattern
+    }
+
+    GameObject CreateLine(Vector3 pos, int id)
+    {
+        var line = GameObject.Instantiate(linePrefab, canvas.transform); 
+        
+        line.transform.localPosition = pos; 
+
+        var lineIdf = line.AddComponent<CircleIdentifier>(); 
+
+        lineIdf.id = id; 
+
+        lines.Add(lineIdf); 
+        return line;  
+
+    }
+    public void OnMouseEnterCircle(CircleIdentifier idf)
+    {
+        Debug.Log(idf.id); 
+    }
+    public void OnMouseExitCircle(CircleIdentifier idf)
+    {
+        Debug.Log(idf.id); 
+    }
+    public void OnMouseDownCircle(CircleIdentifier idf)
+    {
+        Debug.Log(idf.id); 
+
+        unlocking = true; 
+
+        CreateLine(idf.transform.localPosition, idf.id); 
+    }
+    public void OnMouseUpCircle(CircleIdentifier idf)
+    {
+        Debug.Log(idf.id); 
+        unlocking = false; 
     }
 }
